ea8169ffd030ed6b67a7785e5e501c74ffc39dd6
rubensmk <rubenskishimoto@gmail.com>
2021-07-06 21:26:38 -0300

    feat:created task and project models, controllers a migrations

--- start/routes.js
+++ start/routes.js
@@ -7,3 +7,12 @@ Route.post('sessions', 'SessionController.store');
 
 Route.post('passwords', 'ForgotPasswordController.store');
 Route.put('passwords', 'ForgotPasswordController.update');
+
+Route.get('files/:id', 'FileController.show');
+Route.group(() => {
+  Route.post('files', 'FileController.store');
+  Route.resource('projects', 'ProjectController').apiOnly();
+  Route.resource('projects.tasks', 'TaskController').apiOnly();
+}).middleware(['auth'])
+
+
